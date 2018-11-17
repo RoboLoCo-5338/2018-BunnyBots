7a582ec7c016774085f83192253cad67f6d4d52a
AyushHariharan <ayush0624@gmail.com>
2018-11-17 16:34:28 -0500

    started PID tuning

--- robot/src/main/java/org/usfirst/frc/team5338/robot/Robot.java
+++ robot/src/main/java/org/usfirst/frc/team5338/robot/Robot.java
@@ -1,6 +1,7 @@
 //Package for all of our code.
 package org.usfirst.frc.team5338.robot;
 
+
 //Import of necessary subsystem.
 import org.usfirst.frc.team5338.robot.commands.Autonomous;
 import org.usfirst.frc.team5338.robot.subsystems.Claw;
@@ -11,8 +12,10 @@ import org.usfirst.frc.team5338.robot.subsystems.VisionSystem;
 
 //Import of all essential WPILib classes.
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -27,6 +30,7 @@ public class Robot extends IterativeRobot
 	public static final OI oi = new OI();
 	public static final Sensors sensors = new Sensors();
 	public static final VisionSystem vision = new VisionSystem();
+
 	// Creates SendableChooser objects
 	@SuppressWarnings("unused")
 	public static SendableChooser<String> autonomousChooser = new SendableChooser<String>();
@@ -42,6 +46,7 @@ public class Robot extends IterativeRobot
 		// Configures SendableChooser to select Autonomous
 		Robot.setupAutonomous();
 	}
+
 	// Public method that runs once at the beginning of autonomous.
 	@Override
 	public void autonomousInit()
